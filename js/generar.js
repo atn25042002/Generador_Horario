8b02535cc306bce05d221451e10de4e367cc733a
ant25042002 <Aacot@unsa.edu.pe>
2023-10-08 14:04:25 -0500

    Agregando objeto Curso y funcion de llenado, tmb formato

--- js/generar.js
+++ js/generar.js
@@ -1,3 +1,13 @@
+class Curso{
+    constructor(nombre, aula, horas, docente, prioridad){
+        this.nombre= nombre;
+        this.aula= aula;
+        this.horas= horas;
+        this.docente= docente;
+        this.prioridad= prioridad
+    }
+}
+
 var intervalosHorarios = [
     "7:00 AM - 7:50 AM",
     "7:50 AM - 8:40 AM",
@@ -17,10 +27,13 @@ var intervalosHorarios = [
     "8:10 PM - 9:00 PM"
 ];
 
+var cursos= [];
+
 function generarBloques(){
     console.log("generado")
     let tableBody = document.querySelector("#horarioTable tbody");
     let sum= 0;
+    let n= 0;
     intervalosHorarios.forEach(function(intervalo) {
         let row = document.createElement("tr");
         let cell = document.createElement("td");
@@ -28,7 +41,9 @@ function generarBloques(){
         row.appendChild(cell);
         for (var i = 0; i < 5; i++) {
             cell = document.createElement("td");
-            cell.textContent = "Clase " + (i + 1 + sum);
+            n= i + 1 + sum;
+            cell.id= "hora" + n;
+            //cell.textContent = "Clase " + n;
             row.appendChild(cell);
         }
         tableBody.appendChild(row);
@@ -36,7 +51,33 @@ function generarBloques(){
     });
 }
 
+function llenarHorario(){
+    let bloque;
+    /*Object.keys(cursos).forEach(function(curso){
+        cursos[curso].forEach(function(hora){
+            bloque= document.getElementById("hora" + hora);
+            if(bloque.textContent != ""){
+                bloque.style.color="red";
+            }
+            bloque.innerHTML= bloque.innerHTML + curso + "<br>";
+        });
+    });*/
+    console.log(cursos);
+    cursos.forEach(function(curso){
+        curso.horas.forEach(function(hora){
+            bloque= document.getElementById("hora" + hora);
+            if(bloque.textContent != ""){
+                bloque.style.color="red";
+            }
+            bloque.innerHTML= bloque.innerHTML + curso.nombre + "(" + curso.aula + ")" + "<br>";
+        });
+    })
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("iniciado")
+    cursos.push(new Curso("Metodos Numéricos", "306", [1,6],"Olha", true));
+    cursos.push(new Curso("Sistemas Operativos", "306", [2,3,7],"Karim", true));
+    cursos.push(new Curso("Construcción de Software", "306", [3,8],"Arroyo", true));
     generarBloques();
+    llenarHorario();
 });
