dee00c9f2847634628f9b043978cdea6b4e7d0ef
ant25042002 <Aacot@unsa.edu.pe>
2023-10-09 01:10:33 -0500

    Agregando zona de turnos y agregando actualizar

--- js/generar.js
+++ js/generar.js
@@ -9,23 +9,35 @@ class Curso{
 }
 
 var intervalosHorarios = [
-    "7:00 AM - 7:50 AM",
-    "7:50 AM - 8:40 AM",
-    "8:50 AM - 9:40 AM",
-    "9:40 AM - 10:30 AM",
-    "10:40 AM - 11:30 AM",
-    "11:30 AM - 12:20 PM",
-    "12:20 PM - 1:10 PM",
-    "1:10 PM - 2:00 PM",
-    "2:00 PM - 2:50 PM",
-    "2:50 PM - 3:40 PM",
-    "3:50 PM - 4:40 PM",
-    "4:40 PM - 5:30 PM",
-    "5:40 PM - 6:30 PM",
-    "6:30 PM - 7:20 PM",
-    "7:20 PM - 8:10 PM",
-    "8:10 PM - 9:00 PM"
+    "7:00 - 7:50",
+    "7:50 - 8:40",
+    "8:50 - 9:40",
+    "9:40 - 10:30",
+    "10:40 - 11:30",
+    "11:30 - 12:20",
+    "12:20  - 1:10",
+    "1:10  - 2:00",
+    "2:00  - 2:50",
+    "2:50  - 3:40",
+    "3:50  - 4:40",
+    "4:40  - 5:30",
+    "5:40  - 6:30",
+    "6:30  - 7:20",
+    "7:20  - 8:10",
+    "8:10  - 9:00"
 ];
+
+var letra = {
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: 'E',
+    6: 'F',
+    7: 'G',
+    8: 'H',
+    9: 'I',
+};
 
 var cursos= [];
 
@@ -51,6 +63,28 @@ function generarBloques(){
     });
 }
 
+function generarCursos(){    
+    let lstcursos= document.getElementById("col2");
+    cursos.forEach(function(curso){        
+        let label= document.createElement('label');
+        label.textContent= curso.nombre;
+        let turnos= document.createElement('select');
+        turnos.setAttribute('id', curso.nombre);
+        turnos.addEventListener("change", function() {
+            actualizar();
+        });
+        for(let i= 0; i< curso.horas.length; i++){
+            let opcion= document.createElement('option');
+            opcion.text= letra[i+1];
+            opcion.value= i;
+            turnos.appendChild(opcion);
+        }        
+        lstcursos.appendChild(label);
+        lstcursos.appendChild(turnos);
+        lstcursos.append(document.createElement("br"));
+    })
+}
+
 function llenarHorario(){
     let bloque;
     /*Object.keys(cursos).forEach(function(curso){
@@ -64,20 +98,47 @@ function llenarHorario(){
     });*/
     console.log(cursos);
     cursos.forEach(function(curso){
-        curso.horas.forEach(function(hora){
+        let turno= document.getElementById(curso.nombre).value;
+        curso.horas[turno].forEach(function(hora){
             bloque= document.getElementById("hora" + hora);
             if(bloque.textContent != ""){
                 bloque.style.color="red";
             }
-            bloque.innerHTML= bloque.innerHTML + curso.nombre + "(" + curso.aula + ")" + "<br>";
+            bloque.innerHTML= bloque.innerHTML + curso.nombre + "(" + curso.aula[turno] + ")" + "<br>";
         });
     })
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    cursos.push(new Curso("Metodos Numéricos", "306", [1,6],"Olha", true));
-    cursos.push(new Curso("Sistemas Operativos", "306", [2,3,7],"Karim", true));
-    cursos.push(new Curso("Construcción de Software", "306", [3,8],"Arroyo", true));
+function actualizar(){
+    const tablaHTML = `
+    <table id="horarioTable">
+        <thead>
+            <tr>
+                <th id="horarioHora" >Hora</th>
+                <th class="horarioDia">Lunes</th>
+                <th class="horarioDia">Martes</th>
+                <th class="horarioDia">Miércoles</th>
+                <th class="horarioDia">Jueves</th>
+                <th class="horarioDia">Viernes</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Las filas de la tabla serán generadas por JavaScript -->
+        </tbody>
+    </table>`;
+    document.getElementById("horarioTable").innerHTML= tablaHTML;
     generarBloques();
+    llenarHorario();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    cursos.push(new Curso("Metodos Numéricos", ["306","306","306"], [[16,21,26],[43,48,53],[28,33,38]],["Olha","Olha","Olha"], true));
+    cursos.push(new Curso("Sistemas Operativos", ["306","306","306"], [[27,32,14,19],[51,56,44,49],[37,42,24,29]],["Karim","Aceituno","Karim"], true));
+    cursos.push(new Curso("Construcción de Software", ["306","306","306"], [[6,11],[36,41],[17,22],[47,52]],["Arroyo","Arroyo","Edith","Edith"], true));
+    cursos.push(new Curso("Tecnología de objetos", ["306","306","302"], [[54,59,5,10],[71,76,74,79],[72,77,75,80]],["Bornas","Bornas","Sardón"], true));
+    cursos.push(new Curso("Redes", ["205","306","306"], [[28,33,34,39],[57,62,68,73],[67,72,34,39]],["Lucy","Lino","Lino"], true));
+    cursos.push(new Curso("Fundamentos", ["306","306","306"], [[18,23,20,25,30],[58,63,35,40,45],[3,8,13,4,9]],["Juarez","Juarez","Juarez"], true));
+    generarBloques();
+    generarCursos();
     llenarHorario();
 });
