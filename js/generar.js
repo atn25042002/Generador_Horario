6a6d0befed899098482cb5553fff58a12d069f3a
ant25042002 <Aacot@unsa.edu.pe>
2023-10-12 01:40:35 -0500

    Agregando la busqueda en anchura

--- js/generar.js
+++ js/generar.js
@@ -8,7 +8,7 @@ class Curso{
     }
 }
 
-var intervalosHorarios = [
+var intervalosHorarios = [ // Guarda los valores de las horas
     "7:00 - 7:50",
     "7:50 - 8:40",
     "8:50 - 9:40",
@@ -27,7 +27,7 @@ var intervalosHorarios = [
     "8:10  - 9:00"
 ];
 
-var letra = {
+var letra = { //Convierte el Turno del curso a número
     1: 'A',
     2: 'B',
     3: 'C',
@@ -39,11 +39,12 @@ var letra = {
     9: 'I',
 };
 
-var cursos= [];
+var cursos= []; // Contiene los cursos registrados
+var posibles= []; // Guarda los posibles horarios
+var max= 10;
 
 function generarBloques(){
     //Genera el horario con dias y horas vacios
-    console.log("generado")
     let tableBody = document.querySelector("#horarioTable tbody");
     let sum= 0;
     let n= 0;
@@ -87,14 +88,16 @@ function generarCursos(){
         let lblprof= document.createElement('label');
         lblprof.textContent= curso.docente[0];
         lblprof.setAttribute('id', "p-" + i)
+        //Se le asigna los formatos celda y fila para poder mostrarlos con tabulacion en css
         label.setAttribute('class', 'celda');
         turnos.setAttribute('class', 'celda');
         lblprof.setAttribute('class', 'celda');
+        //Agrega el labdel del curso, la casilla select y el label del profesor a la fila
         fila.appendChild(label);
         fila.appendChild(turnos);
         fila.appendChild(lblprof);
 
-        lstcursos.appendChild(fila);
+        lstcursos.appendChild(fila); // Añade la fila al div
         //lstcursos.append(document.createElement("br"));
         i++;
     })
@@ -103,15 +106,16 @@ function generarCursos(){
 function llenarHorario(){
     //Llena el horario con los cursos y sus turnos seleccionados
     let bloque;
-    console.log(cursos);
     let i= 0;
     cursos.forEach(function(curso){
         let turno= document.getElementById('s-' + i).value;
         curso.horas[turno].forEach(function(hora){
             bloque= document.getElementById("hora" + hora);
             if(bloque.textContent != ""){
+                //Si es que hay cruce
                 bloque.style.color="red";
             }
+            //Muestra el aula del turno en el horario y el profesor en la lista de turnos
             bloque.innerHTML= bloque.innerHTML + curso.nombre + "(" + curso.aula[turno] + ")" + "<br>";
             document.getElementById("p-"+i).textContent= curso.docente[turno];
         });
@@ -136,19 +140,75 @@ function actualizar(){
             <!-- Las filas de la tabla serán generadas por JavaScript -->
         </tbody>
     </table>`;
+    //Vuelve a inicializar la tabla
     document.getElementById("horarioTable").innerHTML= tablaHTML;
+    //Vuelve a llenar
     generarBloques();
     llenarHorario();
+    generarHorarios();
 }
+
+function evaluarCruces(turnos){
+    let horasllenas=[];
+    let canthoras = 0;
+    let i= 0;
+    cursos.forEach(function(curso){
+        let turno= turnos[i];
+        let horas= curso.horas[turno];
+        horasllenas.push(...horas);
+        canthoras+= horas.length;
+        i++;
+    })
+    let horasunicas= new Set(horasllenas);
+    return (canthoras - horasunicas.size);
+}
+
+function generarHorarios(){
+    console.log("generando horarios");
+    let turnos= [];
+    let i= 0;
+    cursos.forEach(function(curso){
+        turnos.push(curso.docente.length - 1);
+    })
+    console.log(turnos);
+    let arregloGenerado = new Array(turnos.length);
+    generarArreglosCumpliendoCondicion(turnos, arregloGenerado, 0);
+}
+
+function generarArreglosCumpliendoCondicion(arregloOriginal, arregloGenerado, indice) {
+    if(max <= 0){
+        return;
+    }
+
+    if (indice === arregloOriginal.length) {
+        let cruces= evaluarCruces(arregloGenerado);
+        if(cruces== 0){
+            console.log( cruces + " - " + arregloGenerado);
+            //console.log(arregloGenerado);
+            max--;
+            return;
+        }        
+    }
+
+    for (let i = 0; i <= arregloOriginal[indice]; i++) {
+        arregloGenerado[indice] = i;
+        generarArreglosCumpliendoCondicion(arregloOriginal, arregloGenerado, indice + 1);
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     cursos.push(new Curso("Metodos Numéricos", ["306","306","306"], [[16,21,26],[43,48,53],[28,33,38]],["Olha","Olha","Olha"], true));
     cursos.push(new Curso("Sistemas Operativos", ["306","306","306"], [[27,32,14,19],[51,56,44,49],[37,42,24,29]],["Karim","Aceituno","Karim"], true));
-    cursos.push(new Curso("Construcción de Software", ["306","306","306"], [[6,11],[36,41],[17,22],[47,52]],["Arroyo","Arroyo","Edith","Edith"], true));
+    cursos.push(new Curso("Construcción de Software", ["306","306","306","306"], [[6,11],[36,41],[17,22],[47,52]],["Arroyo","Arroyo","Edith","Edith"], true));
     cursos.push(new Curso("Tecnología de objetos", ["306","306","302"], [[54,59,5,10],[71,76,74,79],[72,77,75,80]],["Bornas","Bornas","Sardón"], true));
     cursos.push(new Curso("Redes", ["205","306","306"], [[28,33,34,39],[57,62,68,73],[67,72,34,39]],["Lucy","Lino","Lino"], true));
     cursos.push(new Curso("Fundamentos", ["306","306","306"], [[18,23,20,25,30],[58,63,35,40,45],[3,8,13,4,9]],["Juarez","Juarez","Juarez"], true));
+    cursos.push(new Curso("Lab- MN", ["306","306","306", "306"], [[54,59],[63,68],[6,11],[34,39]],["Polanco","Polanco","Polanco","Polanco"], true));
+    cursos.push(new Curso("Lab- CS", ["306","306","306"], [[17,22,23,28],[16,21,7,12],[26,31,27,32]],["Polanco","Polanco","Polanco"], true));
+    cursos.push(new Curso("Lab- SO", ["306","306","306", "306"], [[12,17],[11,16],[48,53],[58,63]],["Polanco","Polanco","Polanco", "Polanco"], true));
     generarBloques();
     generarCursos();
     llenarHorario();
+    generarHorarios();
 });
