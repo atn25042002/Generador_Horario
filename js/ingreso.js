ec300df58e7007efe51eb7e51b1ab828a5cd3e73
ant25042002 <Aacot@unsa.edu.pe>
2023-10-31 00:35:37 -0500

    Agregando el generador de turno en el form

--- js/ingreso.js
+++ js/ingreso.js
@@ -1,4 +1,18 @@
+var campo = `
+<label for="docente">Docente:</label>
+<input type="text" name="docente" placeholder="Nombre del docente" required>
+
+<label for="aula">Aula:</label>
+<input type="text" name="aula" placeholder="Nombre del aula" required>
+
+<label for="obligatorio">Obligatorio:</label>
+<input type="checkbox" name="obligatorio">
+`;
+
+//<div class="curso">
+
 document.addEventListener("DOMContentLoaded", function() {
+    agregarTurno();
     generarBloques();
 });
 
@@ -6,3 +20,11 @@ window.addEventListener('beforeunload', function () {
     // Tu código aquí para guardar algo en el LocalStorage justo antes de cerrar la página.
     //localStorage.setItem('cursos', JSON.stringify(cursos));
 })
+
+function agregarTurno(){
+    let campos= document.getElementById('turno-form');
+    let curso= document.createElement('form');
+    curso.innerHTML= campo;
+    curso.className= "turno";
+    campos.appendChild(curso);
+}
