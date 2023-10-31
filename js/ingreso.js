89665365a77054b7ee83f25c81432a6bd0bb371e
ant25042002 <Aacot@unsa.edu.pe>
2023-10-31 01:16:57 -0500

    agregando funcion de agregar horas base

--- js/ingreso.js
+++ js/ingreso.js
@@ -9,11 +9,13 @@ var campo = `
 <input type="checkbox" name="obligatorio">
 `;
 
+var horas=[];
+
 //<div class="curso">
 
 document.addEventListener("DOMContentLoaded", function() {
     agregarTurno();
-    generarBloques();
+    generarBloquesFunc();
 });
 
 window.addEventListener('beforeunload', function () {
@@ -27,4 +29,53 @@ function agregarTurno(){
     curso.innerHTML= campo;
     curso.className= "turno";
     campos.appendChild(curso);
+}
+
+function agregarhora(hora){
+    let zona= document.getElementById('hora' + hora);
+    zona.style.backgroundColor = "blue";
+    horas.push(hora);
+    console.log(horas);
+    zona.onclick = function () {
+        quitarhora(hora);
+    };
+}
+
+function quitarhora(hora){
+    let zona= document.getElementById('hora' + hora);
+    zona.style= '';
+    const index = horas.indexOf(hora);
+    if (index !== -1) {
+        horas.splice(index, 1);
+    }
+    console.log(horas);
+    zona.onclick = function () {
+        agregarhora(hora);
+    };    
+}
+
+function generarBloquesFunc(){
+    //Genera el horario con dias y horas vacios
+    let tableBody = document.querySelector("#horarioTable tbody");
+    let sum= 0;
+    let n= 0;
+    intervalosHorarios.forEach(function(intervalo) {
+        let row = document.createElement("tr");
+        let cell = document.createElement("td");
+        cell.textContent = intervalo;
+        row.appendChild(cell);
+        for (var i = 0; i < 5; i++) {
+            cell = document.createElement("td");
+            n= i + 1 + sum;
+            cell.id= "hora" + n;
+            //cell.setAttribute('onclick',"agregarhora(" + n + ")");
+            cell.onclick = function () {
+                agregarhora(this.id.substring(4));
+            };
+            //cell.textContent = "Clase " + n;
+            row.appendChild(cell);
+        }
+        tableBody.appendChild(row);
+        sum+=5;
+    });
 }
